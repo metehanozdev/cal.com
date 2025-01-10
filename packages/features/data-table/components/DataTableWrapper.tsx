737dfe2d58bfc59248d78490718842fc4da79e58
Eunjae Lee <hey@eunjae.dev>
2025-01-10 17:24:34 +0100

    refactor: use DataTable for /bookings

--- packages/features/data-table/components/DataTableWrapper.tsx
+++ packages/features/data-table/components/DataTableWrapper.tsx
@@ -17,6 +17,8 @@ export type DataTableWrapperProps<TData, TValue> = {
   hasNextPage: boolean;
   fetchNextPage: () => void;
   isFetching: boolean;
+  hideHeader?: boolean;
+  variant?: "default" | "compact";
   totalDBRowCount?: number;
   ToolbarLeft?: React.ReactNode;
   ToolbarRight?: React.ReactNode;
@@ -31,6 +33,8 @@ export function DataTableWrapper<TData, TValue>({
   fetchNextPage,
   isFetching,
   totalDBRowCount,
+  variant,
+  hideHeader,
   ToolbarLeft,
   ToolbarRight,
   children,
@@ -50,6 +54,8 @@ export function DataTableWrapper<TData, TValue>({
       tableContainerRef={tableContainerRef}
       isPending={isPending}
       enableColumnResizing={true}
+      hideHeader={hideHeader}
+      variant={variant}
       onScroll={(e) => fetchMoreOnBottomReached(e.target as HTMLDivElement)}>
       <DataTableToolbar.Root>
         <div className="flex w-full flex-col gap-2 sm:flex-row">
